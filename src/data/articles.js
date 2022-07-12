ec4b086d2fea85e7e871c02e69572e677e89d5e6
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-07-12 18:39:40 -0400

    add article data

---
+++ src/data/articles.js
@@ -0,0 +1,32 @@
+import { imageCurrency, imageRestaurant, imagePlane, imageConfetti} from "../constants/images"
+
+export default [
+    {
+        id: 1,
+        title: "Receive money in any currency with no fees",
+        author: "Claire Robinson",
+        excerpt: "The world is getting smaller and we’re becoming more mobile. So why should you be forced to only receive money in a single …",
+        image: imageCurrency
+    },
+    {
+        id: 2,
+        title: "Treat yourself without worrying about money",
+        author: "Wilson Hutton",
+        excerpt: "Our simple budgeting feature allows you to separate out your spending and set realistic limits each month. That means you …",
+        image: imageRestaurant
+    },
+    {
+        id: 3,
+        title: "Take your Easybank card wherever you go",
+        author: "Wilson Hutton",
+        excerpt: "We want you to enjoy your travels. This is why we don’t charge any fees on purchases while you’re abroad. We’ll even show you …",
+        image: imagePlane
+    },
+    {
+        id: 4,
+        title: "Our invite-only Beta accounts are now live!",
+        author: "Claire Robinson",
+        excerpt: "After a lot of hard work by the whole team, we’re excited to launch our closed beta. It’s easy to request an invite through the site ...",
+        image: imageConfetti
+    },
+]
