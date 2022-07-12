dc0926300b9a0b70c4cd35409b2d12312c0d19a3
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-07-11 23:33:24 -0400

    fix: ghost btn conditional rendering

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -13,38 +13,42 @@ const Navbar = () => {
       </div>
 
       {toggleMenu && (
-        <ul className="navbar__menu" role="list">
-          <li className="navbar__item">
-            <a href="#" className="navbar__link">
-              Home
-            </a>
-          </li>
-          <li className="navbar__item">
-            <a href="#" className="navbar__link">
-              About
-            </a>
-          </li>
-          <li className="navbar__item">
-            <a href="#" className="navbar__link">
-              Contact
-            </a>
-          </li>
-          <li className="navbar__item">
-            <a href="#" className="navbar__link">
-              Blog
-            </a>
-          </li>
-          <li className="navbar__item">
-            <a href="#" className="navbar__link">
-              Careers
-            </a>
-          </li>
-        </ul>
+        <>
+          <ul className="navbar__menu" role="list">
+            <li className="navbar__item">
+              <a href="#" className="navbar__link">
+                Home
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="#" className="navbar__link">
+                About
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="#" className="navbar__link">
+                Contact
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="#" className="navbar__link">
+                Blog
+              </a>
+            </li>
+            <li className="navbar__item">
+              <a href="#" className="navbar__link">
+                Careers
+              </a>
+            </li>
+          </ul>
+          <button className="navbar__ghost-button"></button>
+        </>
       )}
 
-      <button className="navbar__ghost-button"></button>
-
-      <button className="navbar__toggle" onClick={() => setToggleMenu(!toggleMenu)}>
+      <button
+        className="navbar__toggle"
+        onClick={() => setToggleMenu(!toggleMenu)}
+      >
         <img src={iconHamburger} alt="Toggle Menu" />
       </button>
     </nav>
