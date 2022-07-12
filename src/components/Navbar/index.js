46c518bb46e16bd4b4a6cddc3c19d36a7c3d294c
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-07-11 23:21:10 -0400

    mobile menu functionality and styles

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -1,46 +1,50 @@
-import React from "react";
+import React, { useState } from "react";
 import { logo, iconHamburger } from "../../constants/images";
 
-import './styles.scss'
+import "./styles.scss";
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
   return (
     <nav className="navbar">
       <div className="navbar__logo">
         <img src={logo} alt="EasyBank Logo" />
       </div>
 
-      <ul className="navbar__menu" role="list">
-        <li className="navbar__item">
-          <a href="#" className="navbar__link">
-            Home
-          </a>
-        </li>
-        <li className="navbar__item">
-          <a href="#" className="navbar__link">
-            About
-          </a>
-        </li>
-        <li className="navbar__item">
-          <a href="#" className="navbar__link">
-            Contact
-          </a>
-        </li>
-        <li className="navbar__item">
-          <a href="#" className="navbar__link">
-            Blog
-          </a>
-        </li>
-        <li className="navbar__item">
-          <a href="#" className="navbar__link">
-            Careers
-          </a>
-        </li>
-      </ul>
+      {toggleMenu && (
+        <ul className="navbar__menu" role="list">
+          <li className="navbar__item">
+            <a href="#" className="navbar__link">
+              Home
+            </a>
+          </li>
+          <li className="navbar__item">
+            <a href="#" className="navbar__link">
+              About
+            </a>
+          </li>
+          <li className="navbar__item">
+            <a href="#" className="navbar__link">
+              Contact
+            </a>
+          </li>
+          <li className="navbar__item">
+            <a href="#" className="navbar__link">
+              Blog
+            </a>
+          </li>
+          <li className="navbar__item">
+            <a href="#" className="navbar__link">
+              Careers
+            </a>
+          </li>
+        </ul>
+      )}
 
       <button className="navbar__ghost-button"></button>
 
-      <button className="navbar__toggle">
+      <button className="navbar__toggle" onClick={() => setToggleMenu(!toggleMenu)}>
         <img src={iconHamburger} alt="Toggle Menu" />
       </button>
     </nav>
