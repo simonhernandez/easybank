7fd2baa5631beaef0cd16cfeb7159c1cf33ec7e4
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-07-14 02:16:36 -0400

    add aos animation and fix feature jsx structure

--- src/components/Feature/index.js
+++ src/components/Feature/index.js
@@ -4,13 +4,13 @@ import './styles.scss'
 
 const Feature = ({feature}) => {
   return (
-    <li className="feature__item">
+    <div className="feature__item">
       <div className="feature__img-container">
         <img src={feature.image} alt={feature.name} />
       </div>
       <h3 className="heading heading--3">{feature.name}</h3>
       <p>{feature.description}</p>
-    </li>
+    </div>
   );
 };
 
--- src/containers/Features/index.js
+++ src/containers/Features/index.js
@@ -1,13 +1,17 @@
 import React from "react";
 import { Feature } from "../../components";
 
-import './styles.scss';
+import "./styles.scss";
 
 const Features = ({ features }) => {
   return (
     <section className="features">
       <div className="wrapper">
-        <header className="features__header" data-aos="fade" data-aos-duration="1200">
+        <header
+          className="features__header"
+          data-aos="fade"
+          data-aos-duration="1200"
+        >
           <h1 className="heading heading--2">Why choose Easybank?</h1>
           <p>
             We leverage Open Banking to turn your bank account into your
@@ -18,7 +22,15 @@ const Features = ({ features }) => {
         <ul className="features__list" role="list">
           {features.map((feature, index) => {
             return (
-              <Feature feature={feature} key={index}/>
+              <li
+                className="features__item"
+                key={index}
+                data-aos="fade"
+                data-aos-duration="1200"
+                data-aos-delay={`${index * 200}`}
+              >
+                <Feature feature={feature} />
+              </li>
             );
           })}
         </ul>
