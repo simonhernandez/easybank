9598344acaf3cae3dfdaf79baf0e723919caa2d4
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-07-13 21:03:03 -0400

    fix: heading hierachy

--- src/containers/Features/index.js
+++ src/containers/Features/index.js
@@ -8,7 +8,7 @@ const Features = ({ features }) => {
     <section className="features">
       <div className="wrapper">
         <header className="features__header">
-          <h1 className="heading heading--1">Why choose Easybank?</h1>
+          <h1 className="heading heading--2">Why choose Easybank?</h1>
           <p>
             We leverage Open Banking to turn your bank account into your
             financial hub. Control your finances like never before.
