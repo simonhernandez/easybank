b4221951c1ee0a02ef7954eaada8e655b71036da
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-07-14 02:25:44 -0400

    add aos animation and fix feature jsx structure

--- src/containers/Articles/index.js
+++ src/containers/Articles/index.js
@@ -7,13 +7,27 @@ const Articles = ({ articles }) => {
   return (
     <section className="articles">
       <div className="wrapper">
-        <div className="articles__header" data-aos="fade" data-aos-duration="1200">
+        <div
+          className="articles__header"
+          data-aos="fade"
+          data-aos-duration="1200"
+        >
           <h2 className="heading heading--2">Lastest Articles</h2>
         </div>
 
-        <ul className="articles__list">
+        <ul className="articles__list" role="list">
           {articles.map((article, index) => {
-            return <Article article={article} key={index} />;
+            return (
+              <li
+                className="articles__item"
+                key={index}
+                data-aos="fade-up"
+                data-aos-duration="1200"
+                data-aos-delay={`${index * 200}`}
+              >
+                <Article article={article} />
+              </li>
+            );
           })}
         </ul>
       </div>
