c8517a67279d3f4abe111f12752bb3a8f007e84b
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-07-14 22:49:18 -0400

    temp: deactivate animations

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { useEffect } from "react";
 import { Home } from "./pages";
 import AOS from "aos";
-import "aos/dist/aos.css";
+// import "aos/dist/aos.css";
 
 import "./styles/app.scss";
 
 const App = () => {
   
   useEffect(() => {
-    AOS.init();
+    // AOS.init();
   }, []);
 
   return <Home />;
