dc4f92658a1a6f6ccf889151b79e20d78687fb28
Simon Hernandez Barboza <simonbarboza3008@gmail.com>
2022-07-15 19:54:44 -0400

    fix: empty string to features img alt

--- src/components/Feature/index.js
+++ src/components/Feature/index.js
@@ -6,7 +6,7 @@ const Feature = ({feature}) => {
   return (
     <div className="feature__item">
       <div className="feature__img-container">
-        <img src={feature.image} alt={feature.name} />
+        <img src={feature.image} alt="" />
       </div>
       <h3 className="heading heading--3">{feature.name}</h3>
       <p>{feature.description}</p>
